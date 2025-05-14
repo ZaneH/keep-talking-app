6057a6087242a78e5861bc17fdf09ae342ac0b01
Zane Helton <zane99@me.com>
2025-05-14 05:12:49 -0400

    chore: improve models

--- src/components/bomb.tsx
+++ src/components/bomb.tsx
@@ -1,43 +1,301 @@
 import { useGLTF } from "@react-three/drei";
-import { materials } from "./materials";
+// import { materials } from "./materials";
 
 export default function Bomb() {
-  const { nodes, scene } = useGLTF("./bomb.glb") as any;
-  scene.traverse((child: any) => {
-    if (child.isMesh) {
-      child.castShadow = true;
-      child.receiveShadow = true;
-    }
-  });
+  const { nodes, materials } = useGLTF("/bomb.glb") as any;
 
   return (
-    <group dispose={null} position={[0, 0.75, 0]}>
+    <group dispose={null} position={[0, 0.73, 0]}>
       <mesh
-        geometry={nodes.Cube029.geometry}
+        castShadow
+        receiveShadow
+        geometry={nodes.WiresModule.geometry}
         material={materials.Silver}
-        castShadow
-      />
+        position={[0.197, -0.101, 0.074]}
+        scale={[1.028, 1.028, 1]}
+      >
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.BigStrip.geometry}
+          material={materials["Plastic Dark"]}
+          position={[-0.055, 0.052, 0.021]}
+        >
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Plug1.geometry}
+            material={materials["Plastic Light"]}
+            position={[0.001, 0.002, 0.013]}
+            scale={0.482}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Plug2.geometry}
+            material={materials["Plastic Light"]}
+            position={[0.001, -0.019, 0.013]}
+            scale={0.482}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Plug3.geometry}
+            material={materials["Plastic Light"]}
+            position={[0.001, -0.041, 0.013]}
+            scale={0.482}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Plug4.geometry}
+            material={materials["Plastic Light"]}
+            position={[0.001, -0.062, 0.013]}
+            scale={0.482}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Plug5.geometry}
+            material={materials["Plastic Light"]}
+            position={[0.001, -0.084, 0.013]}
+            scale={0.482}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Plug6.geometry}
+            material={materials["Plastic Light"]}
+            position={[0.001, -0.105, 0.013]}
+            scale={0.482}
+          />
+        </mesh>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.SmallStrip.geometry}
+          material={materials["Plastic Dark"]}
+          position={[0.031, 0.052, 0.021]}
+        >
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Plug1001.geometry}
+            material={materials["Plastic Light"]}
+            position={[0, -0.017, 0.013]}
+            rotation={[0, 0, Math.PI]}
+            scale={0.482}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Plug2001.geometry}
+            material={materials["Plastic Light"]}
+            position={[0, -0.035, 0.013]}
+            rotation={[0, 0, Math.PI]}
+            scale={0.482}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Plug3001.geometry}
+            material={materials["Plastic Light"]}
+            position={[0, -0.052, 0.013]}
+            rotation={[0, 0, Math.PI]}
+            scale={0.482}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Plug4001.geometry}
+            material={materials["Plastic Light"]}
+            position={[0, -0.07, 0.013]}
+            rotation={[0, 0, Math.PI]}
+            scale={0.482}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Plug5001.geometry}
+            material={materials["Plastic Light"]}
+            position={[0, -0.088, 0.013]}
+            rotation={[0, 0, Math.PI]}
+            scale={0.482}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Plug6001.geometry}
+            material={materials["Plastic Light"]}
+            position={[0, -0.105, 0.013]}
+            rotation={[0, 0, Math.PI]}
+            scale={0.482}
+          />
+        </mesh>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Wire1.geometry}
+          material={nodes.Wire1.material}
+          position={[-0.008, 0.04, 0.037]}
+          rotation={[-Math.PI, 0, 0.243]}
+          scale={0.05}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Wire2.geometry}
+          material={nodes.Wire2.material}
+          position={[-0.008, 0.023, 0.037]}
+          rotation={[-Math.PI, 0, 0.243]}
+          scale={0.05}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Wire3.geometry}
+          material={nodes.Wire3.material}
+          position={[-0.008, 0.006, 0.037]}
+          rotation={[-Math.PI, 0, 0.243]}
+          scale={0.05}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Wire4.geometry}
+          material={nodes.Wire4.material}
+          position={[-0.008, -0.033, 0.037]}
+          rotation={[-Math.PI, 0, 0.243]}
+          scale={0.05}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Wire5.geometry}
+          material={nodes.Wire5.material}
+          position={[-0.008, -0.016, 0.037]}
+          rotation={[-Math.PI, 0, 0.243]}
+          scale={0.05}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Wire6.geometry}
+          material={nodes.Wire6.material}
+          position={[-0.008, -0.053, 0.037]}
+          rotation={[-Math.PI, 0, 0.243]}
+          scale={0.05}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Light.geometry}
+          material={materials["Solved light"]}
+          position={[0.059, 0.058, 0.021]}
+          rotation={[Math.PI / 2, 0, 0]}
+          scale={0.012}
+        />
+      </mesh>
       <mesh
-        geometry={nodes.Cube029_1.geometry}
-        material={materials["Darker Orange"]}
         castShadow
+        receiveShadow
+        geometry={nodes.Case.geometry}
+        material={materials.Silver}
+        scale={[0.911, 1, 1]}
+      >
+        <group position={[0, -0.732, 0]} scale={[1.098, 1, 1]}>
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.H.geometry}
+            material={materials.Silver}
+            position={[0, 0.732, 0]}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.H001.geometry}
+            material={materials.Silver}
+            position={[0, 0.732, 0]}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.H002.geometry}
+            material={materials.Silver}
+            position={[0.001, 0.732, 0]}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.V.geometry}
+            material={materials.Silver}
+            position={[0.005, 0.732, 0]}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.V003.geometry}
+            material={materials.Silver}
+            position={[-0.007, 0.732, 0]}
+          />
+        </group>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Backplate.geometry}
+          material={materials.Silver}
+          scale={[1.098, 1, 1]}
+        />
+      </mesh>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.ButtonModule.geometry}
+        material={materials.Silver}
+        position={[-0.001, -0.101, 0.074]}
+        scale={[1.028, 1.028, 1]}
+      >
+        <group position={[0.002, 0.096, -0.074]} scale={[0.943, 0.943, 1]}>
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.ButtonHead.geometry}
+            material={materials["Button Red"]}
+            position={[0, -0.108, 0.127]}
+            rotation={[Math.PI / 2, 0, 0]}
+            scale={[0.043, 0.006, 0.043]}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.ButtonShaft.geometry}
+            material={materials["Button Red"]}
+            position={[0, -0.108, 0.113]}
+            rotation={[Math.PI / 2, 0, 0]}
+            scale={[0.032, 0.011, 0.032]}
+          />
+        </group>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Light001.geometry}
+          material={materials["Unlit light"]}
+          position={[0.054, 0.057, 0.021]}
+          rotation={[Math.PI / 2, 0, 0]}
+          scale={[0.011, 0.012, 0.011]}
+        />
+      </mesh>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Clock.geometry}
+        material={materials["Silver Dark"]}
+        position={[-0.197, -0.101, 0.074]}
+        scale={[1.028, 1.028, 1]}
       />
-      <group position={[0, -0.733, 0]}>
-        <mesh geometry={nodes.Cube016.geometry} material={materials.Silver} />
-        <mesh geometry={nodes.Cube017.geometry} material={materials.Silver} />
-        <mesh geometry={nodes.Cube018.geometry} material={materials.Silver} />
-        <mesh geometry={nodes.Cube019.geometry} material={materials.Silver} />
-        <mesh geometry={nodes.Cube020.geometry} material={materials.Silver} />
-      </group>
-      <group position={[0, -0.733, 0]} rotation={[Math.PI, 0, Math.PI]}>
-        <mesh geometry={nodes.Cube011.geometry} material={materials.Silver} />
-        <mesh geometry={nodes.Cube012.geometry} material={materials.Silver} />
-        <mesh geometry={nodes.Cube013.geometry} material={materials.Silver} />
-        <mesh geometry={nodes.Cube014.geometry} material={materials.Silver} />
-        <mesh geometry={nodes.Cube015.geometry} material={materials.Silver} />
-      </group>
     </group>
   );
 }
 
-useGLTF.preload("./bomb.glb");
+useGLTF.preload("/bomb.glb");
--- src/components/scene.tsx
+++ src/components/scene.tsx
@@ -1,66 +1,104 @@
 "use client";
 
-import { GrpcWebFetchTransport } from "@protobuf-ts/grpcweb-transport";
-import { Environment, PerspectiveCamera, useGLTF } from "@react-three/drei";
-import { useThree } from "@react-three/fiber";
-import { DepthOfField, EffectComposer } from "@react-three/postprocessing";
-import { useEffect } from "react";
-import { GameServiceClient } from "../generated/proto/game.client";
+import { Environment, PerspectiveCamera } from "@react-three/drei";
+import {
+  BrightnessContrast,
+  EffectComposer,
+} from "@react-three/postprocessing";
+import { useRef } from "react";
 import Bomb from "./bomb";
-import { materials } from "./materials";
+import Table from "./table";
 
 export default function Scene() {
-  const { camera, gl } = useThree();
-  useEffect(() => {
-    // TODO: Might be useless. PerspectiveCamera has a aspect property
-    const onResize = () => {
-      (camera as any).aspect = window.innerWidth / window.innerHeight;
-      camera.updateProjectionMatrix();
+  const lightRef = useRef<any>(null);
+  const lightRef2 = useRef<any>(null);
+  const lightRef3 = useRef<any>(null);
+  const lightRef4 = useRef<any>(null);
+  const lightRef5 = useRef<any>(null);
 
-      gl.setSize(window.innerWidth, window.innerHeight);
-    };
+  // useEffect(() => {
+  //   (async () => {
+  //     const transport = new GrpcWebFetchTransport({
+  //       baseUrl: "http://localhost:8080",
+  //     });
+  //     const gameService = new GameServiceClient(transport);
+  //     const response = await gameService.createGame({});
+  //     console.log(response.response);
+  //   })();
+  // }, []);
 
-    onResize();
-    (async () => {
-      const transport = new GrpcWebFetchTransport({
-        baseUrl: "http://localhost:8080",
-      });
-      const gameService = new GameServiceClient(transport);
-      const response = await gameService.createGame({});
-      console.log(response.response);
-    })();
-
-    window.addEventListener("resize", onResize);
-    return () => {
-      window.removeEventListener("resize", onResize);
-    };
-  }, []);
+  // useHelper(lightRef, DirectionalLightHelper);
+  // useHelper(lightRef2, DirectionalLightHelper);
+  // useHelper(lightRef3, RectAreaLightHelper, "blue");
+  // useHelper(lightRef4, RectAreaLightHelper, "green");
+  // useHelper(lightRef5, RectAreaLightHelper, "red");
 
   return (
     <>
       <group>
-        <directionalLight position={[2, 3, 1.5]} intensity={1} castShadow />
-        <directionalLight position={[0.2, 0.8, 0.5]} intensity={1} castShadow />
+        <directionalLight
+          position={[1, 0.7, -1.1]}
+          scale={[0.5, 0.5, 0.5]}
+          intensity={0.3}
+          castShadow
+          shadow-normalBias={0.05}
+          ref={lightRef}
+        />
+        <directionalLight
+          position={[1, 1.9, 1.1]}
+          scale={[0.5, 0.5, 0.5]}
+          intensity={1.0}
+          castShadow
+          shadow-normalBias={0.05}
+          ref={lightRef5}
+        />
+        <directionalLight
+          position={[-0.9, 1.9, -1.0]}
+          intensity={0.8}
+          scale={[0.5, 0.5, 0.5]}
+          castShadow
+          shadow-normalBias={0.05}
+          ref={lightRef2}
+        />
+        <ambientLight intensity={0.2} />
+        <pointLight
+          position={[0, 1.5, 0]}
+          intensity={0.6}
+          scale={[0.5, 0.5, 0.5]}
+          castShadow
+          shadow-normalBias={0.05}
+          ref={lightRef3}
+        />
         <rectAreaLight
-          intensity={3}
-          position={[2, 2.8, 0]}
+          intensity={0.8}
+          position={[0, 1.8, 0]}
           height={2}
           width={3}
           rotation={[Math.PI / -3, Math.PI / 8, Math.PI / 8]}
+          ref={lightRef3}
         />
         <rectAreaLight
-          intensity={3}
-          position={[-1, 3, 0]}
-          height={2}
+          intensity={0.7}
+          position={[-1, 1.5, 0]}
+          height={0.8}
           width={3}
           rotation={[Math.PI / 2.3, Math.PI / -1.2, Math.PI / 2]}
+          ref={lightRef5}
         />
-        <rectAreaLight
+        {/* <rectAreaLight
           intensity={4}
           position={[0, 0, 4]}
           height={2}
           width={3}
           rotation={[Math.PI / 1.1, Math.PI / -1.2, 0]}
+        /> */}
+        <rectAreaLight
+          intensity={1.2}
+          position={[1, 1.9, -1]}
+          height={1}
+          width={1}
+          rotation={[Math.PI / 2, Math.PI, Math.PI / 2]}
+          ref={lightRef4}
         />
       </group>
       <group position={[0, -0.8, 0]}>
@@ -70,117 +108,17 @@ export default function Scene() {
 
       <Environment preset="night" background blur={0.8} />
       <EffectComposer>
-        <DepthOfField focusDistance={0.01} focalLength={0.1} bokehScale={8} />
+        {/* <DepthOfField focusDistance={0.01} focalLength={0.1} bokehScale={2} /> */}
+        <BrightnessContrast contrast={0.15} />
       </EffectComposer>
       <PerspectiveCamera
         makeDefault
         position={[0, 0, 10]}
         rotation={[-Math.PI / 8, 0, 0]}
         fov={60}
-        near={0.1}
+        near={0.2}
         far={50}
       />
     </>
   );
 }
-
-function Table() {
-  const { nodes, scene } = useGLTF("/table-room.glb") as any;
-  scene.traverse((child: any) => {
-    if (child.isMesh) {
-      child.castShadow = true;
-      child.receiveShadow = true;
-    }
-  });
-
-  return (
-    <group>
-      <group dispose={null}>
-        <group position={[0, 0.916, -1.674]}>
-          <mesh
-            geometry={nodes.Bars.geometry}
-            material={materials["Darker Orange"]}
-            scale={[0.106, 1.058, 0.106]}
-          />
-          <mesh
-            geometry={nodes.Frame.geometry}
-            material={materials["Darker Orange"]}
-            position={[0, 0.758, 0]}
-            rotation={[Math.PI / 2, 0, 0]}
-            scale={[5.492, 12, 5.492]}
-          />
-          <mesh
-            geometry={nodes.Glass.geometry}
-            material={materials["Glass"]}
-            position={[0, -0.002, 0]}
-            scale={[1.011, 1.011, 0.01]}
-          />
-        </group>
-        <mesh
-          geometry={nodes.Roof.geometry}
-          material={materials["Orange PBSR"]}
-          position={[0, 2.73, 0]}
-          rotation={[0, 0, Math.PI]}
-          scale={[5.492, 12, 5.492]}
-        />
-        <mesh
-          geometry={nodes.Floor.geometry}
-          material={materials["Orange PBSR"]}
-          scale={[5.492, 12, 5.492]}
-          receiveShadow
-        />
-        <mesh
-          geometry={nodes.WallAccent.geometry}
-          material={materials["Darker Orange"]}
-          position={[1.674, 0.369, 0]}
-        />
-        <mesh
-          geometry={nodes.WallB.geometry}
-          material={materials["Orange PBSR"]}
-          position={[0, 1.674, -1.674]}
-          rotation={[Math.PI / 2, 0, 0]}
-          scale={[5.492, 12, 5.492]}
-        />
-        <mesh
-          geometry={nodes.WallF.geometry}
-          material={materials["Orange PBSR"]}
-          position={[0, 1.674, 1.674]}
-          rotation={[Math.PI / 2, 0, Math.PI]}
-          scale={[5.492, 12, 5.492]}
-        />
-        <mesh
-          geometry={nodes.WallL.geometry}
-          material={materials["Orange PBSR"]}
-          position={[-1.674, 1.674, 0]}
-          rotation={[Math.PI / 2, 0, -Math.PI / 2]}
-          scale={[5.492, 12, 5.492]}
-        />
-        <mesh
-          geometry={nodes.WallR.geometry}
-          material={materials["Orange PBSR"]}
-          position={[1.674, 1.674, 0]}
-          rotation={[Math.PI / 2, 0, Math.PI / 2]}
-          scale={[5.492, 12, 5.492]}
-        />
-        <mesh
-          geometry={nodes.Legs.geometry}
-          material={materials["Orange PBSR"]}
-          position={[0.655, 0, -0.335]}
-          scale={0.184}
-          castShadow
-          receiveShadow
-        />
-        <mesh
-          geometry={nodes.Top.geometry}
-          material={materials["Orange PBSR"]}
-          position={[0, 0.449, 0]}
-          scale={0.184}
-          castShadow
-          receiveShadow
-        />
-      </group>
-    </group>
-  );
-}
-
-useGLTF.preload("/table-room.glb");
