4e20619e202eb117adc1774b622a537406087e93
Zane Helton <zane99@me.com>
2025-05-19 20:58:53 -0400

    chore: improve general performance

--- src/components/modules/simon-says-module.tsx
+++ src/components/modules/simon-says-module.tsx
@@ -22,43 +22,42 @@ export default function SimonSaysModule({ id = "simon-says" }: ModuleProps) {
         <mesh
           castShadow
           receiveShadow
-          geometry={nodes.Light003.geometry}
-          material={materials["Unlit light"]}
-          position={[0.061, 0.062, 0.021]}
-          rotation={[Math.PI / 2, 0, 0]}
-          scale={[0.011, 0.012, 0.011]}
-        />
-        <mesh
-          castShadow
-          receiveShadow
           geometry={nodes.SimonSaysPatternD.geometry}
           material={materials.GreenLight}
-          position={[0, -0.034, 0.032]}
-          scale={[1.033, 1.033, 1.062]}
+          position={[0, -0.035, 0.032]}
+          scale={1.062}
         />
         <mesh
           castShadow
           receiveShadow
           geometry={nodes.SimonSaysPatternL.geometry}
           material={materials.RedLight}
-          position={[-0.033, 0, 0.032]}
-          scale={[1.033, 1.033, 1.062]}
+          position={[-0.034, 0, 0.032]}
+          scale={1.062}
         />
         <mesh
           castShadow
           receiveShadow
           geometry={nodes.SimonSaysPatternR.geometry}
-          material={materials.WireYellow}
-          position={[0.033, 0, 0.032]}
-          scale={[1.033, 1.033, 1.062]}
+          material={materials.YellowLight}
+          position={[0.034, 0, 0.032]}
+          scale={1.062}
         />
         <mesh
           castShadow
           receiveShadow
           geometry={nodes.SimonSaysPatternU.geometry}
           material={materials.BlueLight}
-          position={[0, 0.034, 0.032]}
-          scale={[1.033, 1.033, 1.062]}
+          position={[0, 0.035, 0.032]}
+          scale={1.062}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Light003.geometry}
+          material={materials["Unlit light"]}
+          position={[0.062, 0.064, 0.021]}
+          rotation={[Math.PI / 2, 0, 0]}
         />
       </mesh>
     </Module>
