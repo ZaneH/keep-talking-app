c441e48087e6b44d5fe9cbd1782d81c8f66ba732
Zane Helton <zane99@me.com>
2025-05-20 18:14:00 -0400

    feat: plug simple wire into grpc server

---
+++ src/components/modules/custom-materials.ts
@@ -0,0 +1,8 @@
+import * as THREE from "three";
+export const CustomMaterials = {
+  GreenLight: new THREE.MeshStandardMaterial({
+    color: 0x00c136,
+    emissive: 0x00c136,
+    emissiveIntensity: 0.45,
+  }),
+};
