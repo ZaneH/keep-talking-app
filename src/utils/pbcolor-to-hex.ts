47c6c7bd4808a6530c2912a371d6c912359a23d2
Zane Helton <zane99@me.com>
2025-05-21 20:08:49 -0400

    feat: add color to big button

---
+++ src/utils/pbcolor-to-hex.ts
@@ -0,0 +1,25 @@
+import { Color } from "../generated/proto/common";
+
+export function pbColorToHex(color: Color): number {
+  switch (color) {
+    case Color.BLACK:
+      return 0x000000;
+    case Color.BLUE:
+      return 0x0000ff;
+    case Color.WHITE:
+      return 0xffffff;
+    case Color.RED:
+      return 0xff0000;
+    case Color.GREEN:
+      return 0x00ff00;
+    case Color.YELLOW:
+      return 0xffff00;
+    case Color.ORANGE:
+      return 0xffa500;
+    case Color.PINK:
+      return 0xffc0cb;
+    default:
+      console.error("Invalid color value:", color);
+      return 0xffffff;
+  }
+}
